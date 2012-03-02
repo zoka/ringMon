656a79f55ea64b184537840ac2d394fafaa25691
zoka <ztomicic@gmail.com>
2012-03-02 16:06:02 +1100

    transient repl sessions ok - work in progress on permanent ones

--- src/noirmon/models/nrepl.clj
+++ src/noirmon/models/nrepl.clj
@@ -2,11 +2,54 @@
   (:require [clojure.tools.nrepl.server    :as server]
             [clojure.tools.nrepl.misc      :as misc]
             [clojure.tools.nrepl.transport :as t]
-            [clojure.tools.nrepl           :as repl])
+            [clojure.tools.nrepl           :as repl]
+            [clojure.tools.nrepl.misc      :as repl.misc]
+            (clojure walk))
   (:import  (clojure.tools.nrepl.transport FnTransport)
-             (java.util.concurrent 
+             (java.util.concurrent
                 LinkedBlockingQueue
                 TimeUnit)))
+
+(defn set-to-vec
+ "Convert set containing keywords into
+  vector of strings."
+  [s]
+  (let [v (map #(name %1) s)]
+    (vec v)))
+
+(defn patch-from-srv-msg
+ "Patch message sent by nREPL server before it gets
+  delivered to client.
+  First keywordize everything.
+  Second - the status comes back as a set and needs to be
+  converted to vector and all keywords in it
+  to strings, since bencoder seems to be
+  doing the same thing"
+  [msg]
+  (let [k (clojure.walk/keywordize-keys msg)
+        s (get k :status)]
+
+    (println "srv-> original:" msg)
+    (if s
+      (let [p (assoc k :status (set-to-vec s))]
+        (println "srv-> patched:" p)
+        p)
+      (do
+        (println "srv-> patched:" k)
+        k))))
+
+(defn patch-from-client-msg
+ "Patch message sent by client
+  before it gets delivered to nREPL server.
+  If there are any map values that are keywords
+  convert them to plain strings, so they can be
+  properly interpreted on server side."
+  [msg]
+  (let [p (into {}
+            (for [[k v]  msg] [ k (#(name %1) v) ]))]
+     (println "clnt-> original:" msg)
+     (println "clnt-> patched:" p)
+     p))
 
 (defn transport-pair
  "Returns vector of 2 direct transport instances,
@@ -19,16 +62,15 @@
   (let [client-q (LinkedBlockingQueue.)
        server-q  (LinkedBlockingQueue.)
        open (atom true)  ; all these variables remain in closure
-                         ; so they hold instance state in a safe place                
-                    
+                         ; so they hold instance state in a safe place
+
        client-transport (FnTransport.
          (fn read [timeout]
-           (if @open 
+           (if @open
              (let [data (.poll server-q timeout TimeUnit/MILLISECONDS)]
-               (println "client got data:" data)
-               (when-not @open 
-                 (.clear server-q)) ; clear q if it was closed while 
-               data)))              ; we were blocked in poll
+               (when-not @open
+                 (.clear server-q)) ; clear q if it was closed while we were blocked in poll
+               (patch-from-srv-msg data))))
          (fn write [data]
            (when @open
              (.put client-q data)))
@@ -37,21 +79,20 @@
            (.clear server-q)))  ; safe to clear incomming queue
 
        server-transport (FnTransport.
-         (fn read [timeout]    
-           (if @open 
+         (fn read [timeout]
+           (if @open
              (let [data (.poll client-q timeout TimeUnit/MILLISECONDS)]
-               (println "server got data:" data)
-               (when-not @open 
+               (when-not @open
                  (.clear client-q))
-               data)))
+               (patch-from-client-msg data))))
          (fn write [data]
            (when @open
              (.put server-q data)))
          (fn close []
            (reset! open false)
            (.clear client-q)))]  ; safe to clear incomming queue
-  [client-transport server-transport]))  
-       
+  [client-transport server-transport]))
+
 (defn handle
   "Handles requests received via `transport` using `handler`.
    Returns nil when `recv` returns nil for the given transport."
@@ -68,27 +109,90 @@
 (defn connect
   "Connects to an REPL within the same procees using
    pair of LinkedBlockedQueue instances.
-   There is no server here, just a 
+   There is no server here, just a
    furure that handles one to one connection.
    Returns client transport instance."
-  [] 
+  []
   (let [ [client-t server-t] (transport-pair) ]
-    (future (with-open 
+    (future (with-open
               [transport server-t
                handler   (server/default-handler)]
-               (println "In future")
               (handle handler transport)))
     client-t))
 
+
+(def sessions (atom {}))
 
 ;; transient session
 (defn handle-transient [req]
   (println "orig request:" req)
   (with-open [conn (connect)]
-    (println "conn" conn) 
-    (-> (repl/client conn 100) 
+    (-> (repl/client conn 5000)
       (repl/message req)
       doall)))
+
+
+(defprotocol SessionOps
+  (poll [this] [this timeout]
+    "Reads and returns accumulated session output. Will block if timeout is
+     nonzero. Should return nil if message is not available after `timeout`
+     ms.")
+  (put [this cmd] "Sends msg. Implementations should return the transport.")
+  (get-id [this] "Gets session id"))
+
+(deftype FnSession [poll-fn put-fn get-id-fn]
+  SessionOps
+  (put  [this cmd]  (put-fn cmd))
+  (poll [this] (.poll this 0))
+  (poll [this timeout] (poll-fn timeout))
+  (get-id [this] (get-id-fn)))
+
+(defn get-new-session
+  [client conn]
+  (println "client:" client "\nconn:" conn) (Thread/sleep 1000)
+  (repl/new-session client))
+
+(defn session-instance []
+  (let [conn     (connect)
+        client   (repl/client conn 100)
+        sid      (get-new-session client conn)
+        resp-seq (repl/client-session {:session sid})]
+
+    (FnSession.
+      (fn poll [timeout]
+        (doall resp-seq))
+      (fn put [cmd]
+        (let [cid (repl.misc/uuid)]
+          (repl/message
+            client
+            (assoc cmd :session sid :id cid))
+        cid))
+      (fn get-id []
+        sid))))
+
+(defn init-session
+  []
+  (let [s (session-instance)]
+    (when s
+      (swap! sessions assoc (get-id s) s))
+        (get-id s)))
+
+(defn session-put
+  [sid cmd]
+  (let [s (get @sessions sid)]
+    (when s
+      (put s cmd))))
+
+(defn session-poll
+  [sid]
+  (let [s (get @sessions sid)]
+    (when s
+      (poll s))))
+
+
+
+
+
 
 
 
--- src/noirmon/views/monitor.clj
+++ src/noirmon/views/monitor.clj
@@ -1,6 +1,6 @@
 (ns noirmon.views.monitor
   (:require [noir.response :as resp]
-            [noir.request :as req]
+            [noir.session :as session]
             [noirmon.models.nrepl :as repl]
             [clojure.java.jmx :as jmx])
   (:use [noir.core :only [defpage]]))
@@ -12,13 +12,16 @@
   
 (def ^:const sample-interval 2000) ; msec
 
-(defn get-process-nanos []
+(defn get-process-nanos
+  []
   (jmx/read "java.lang:type=OperatingSystem" :ProcessCpuTime))
 
-(defn calc-cpu-load [cpu-time clock-time]
+(defn calc-cpu-load
+  [cpu-time clock-time]
   (/ (* 100.0 cpu-time) clock-time))
 
-(defn data-sampler []
+(defn data-sampler
+  []
   (loop [process-nanos     (get-process-nanos)
          real-nanos        (System/nanoTime)
          ajax-reqs         @ajax-reqs-tot
@@ -42,14 +45,16 @@
                 real-nanos
                 ajax-reqs)))
 
-(defn init []
+(defn init
+  []
   ; kick off endless data-sampler thread
   ; has to be called from noirmon.server/-main
   (.start (Thread. data-sampler))
-  ) ; init nrepl  
+  ) 
    
 
-(defn get-mon-data []
+(defn get-mon-data
+  []
   (let [os  (jmx/mbean "java.lang:type=OperatingSystem")
         mem (jmx/mbean "java.lang:type=Memory")
         ; java.jmx returns Java arrays which repl/json can not handle
@@ -64,29 +69,52 @@
           :Memory          mem
           :Threading       th}))
 
-(defn do-jvm-gc []
+(defn do-jvm-gc
+  []
   (jmx/invoke "java.lang:type=Memory" :gc)
   {:resp "ok"})
 
-(defn do-repl [code]
-  (let [r (repl/handle-transient {:op "eval" :code code})] 
+(defn get-sess-id
+  []
+  (let [id (session/get :repl-sess-id)]
+    (if id
+      id
+      (let [id (repl/init-session)]
+        (session/put! :repl-sess-id id)
+        id))))
+
+(defn do-repl
+  [code]
+  (let [sid (get-sess-id)
+        cid (repl/session-put sid {:op :eval :code code})
+        r {:cid cid}]
     (println "do-repl:" r)
     r))
+    
+(defn do-transient-repl
+  [code]
+  (let [r  (repl/handle-transient {:op :eval :code code})]
+    (println "do-transient-repl:" r)
+    r))
 
-(defn decode-cmd [request]
+(defn decode-cmd 
+  [request]
   (let [cmd (keyword (:cmd request))]
     (swap! ajax-reqs-tot inc)  
     (case cmd
       :get-mon-data (get-mon-data)
-      :do-jvm-gc (do-jvm-gc)
-      :do-repl (do-repl (:code request))
+      :do-jvm-gc    (do-jvm-gc)
+      :do-repl      (do-repl (:code request))
       {:resp "bad-cmd"})))
 
+;-------------------------------------------------------
 
-(defpage "/admin/monview" []
+(defpage "/admin/monview"
+  []
   (resp/redirect "/admin/monview.html"))
 
-(defpage [:get "/admin/moncmd"] {:as params}
+(defpage 
+  [:get "/admin/moncmd"] {:as params}
   (let [reply (decode-cmd params)]
     (resp/json reply)))
 
